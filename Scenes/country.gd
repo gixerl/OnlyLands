545f0d5268a6f9441647a53cf814b2d212d3fefa
Ikaikuro <benes@belliatus.de>
2025-06-09 00:27:20 +0200

    Character, Info und mehr

--- Scenes/country.gd
+++ Scenes/country.gd
@@ -1,9 +1,24 @@
 extends Area2D
 
 
+@onready var characters = get_tree().root.get_node("Main/CanvasLayer/Characters")
+@onready var info = get_tree().root.get_node("Main/CanvasLayer/Info")
+@onready var infotext = get_tree().root.get_node("Main/CanvasLayer/Informationen")
+
+var new_texture
+var new_text
+
 func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.button_index==MOUSE_BUTTON_LEFT and event.pressed:
 		for node in get_parent().get_children():
 			node.modulate = Color(1,1,1)
 		self.modulate = Color(1,0,0)
-		print(name)
+		new_texture = load("res://Assets/Characters/%s.png" % name.to_lower())
+		new_text = FileAccess.open("res://Assets/Infos/%s.txt" % name.to_lower(), FileAccess.READ)
+		new_text = new_text.get_as_text()
+		print(characters)
+		characters.visible = true
+		info.visible = true
+		characters.texture = new_texture
+		infotext.text = new_text
+		
---
+++ Scenes/main_menu.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+@onready var text_edit: TextEdit = $CanvasLayer/Informationen
+
+
+
+func _on_button_pressed() -> void:
+	if text_edit.visible:
+		text_edit.visible = false
+	else:
+		text_edit.visible = true
