11e6e1ac6da470d8511e001c28f5e6f5c5cdcda9
max <maxim.desanctis@gmail.com>
2025-06-11 12:50:07 +0200

    Added selection screen

--- Scenes/country.gd
+++ Scenes/country.gd
@@ -1,7 +1,9 @@
 extends Area2D
 var country_color = Color(1,1,1)
 
+
 @onready var characters = get_tree().root.get_node("Main/CanvasLayer/Characters")
+@onready var enemies = get_tree().root.get_node("CanvasLayer/Characters")
 @onready var info = get_tree().root.get_node("Main/CanvasLayer/Info")
 @onready var infotext = get_tree().root.get_node("Main/CanvasLayer/Informationen")
 
@@ -9,18 +11,41 @@ var new_texture
 var new_text
 func _ready():
 	self.modulate = country_color
+	if name == GameState.selected_country:
+		self.modulate = Color(1, 0, 0)
 
 func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.button_index==MOUSE_BUTTON_LEFT and event.pressed:
 		for node in get_parent().get_children():
 			if "country_color" in node:
-				node.modulate = node.country_color
-		self.modulate = Color(1,0,0)
-		new_texture = load("res://Assets/Characters/%s.png" % name)
-		new_text = FileAccess.open("res://Assets/Infos/%s.txt" % name, FileAccess.READ)
-		new_text = new_text.get_as_text()
-		characters.visible = true
-		info.visible = true
-		characters.texture = new_texture
-		infotext.text = new_text
+				# the own country cannot be changed after pressing the start button
+				# TODO remove
+				if node.name == GameState.selected_country and GameState.country_locked:
+					continue
+				elif node.name == GameState.selected_enemy and GameState.enemy_locked:
+					continue
+				else:
+					node.modulate = node.country_color
+		self.modulate = Color(1, 0, 0)
+		if not GameState.country_locked:
+			GameState.selected_country = name
+		elif not GameState.enemy_locked:
+			GameState.selected_enemy = name
+		else:
+			return
+		if characters != null:
+			new_texture = load("res://Assets/Characters/%s.png" % name)
+			characters.visible = true
+			characters.texture = new_texture
+		else:
+			assert(GameState.country_locked == true)	# should be in selection screen
+			enemies = get_tree().root.get_node("Selection_screen/CanvasLayer/Enemies")
+			new_texture = load("res://Assets/Characters/%s.png" % name)
+			enemies.visible = true
+			enemies.texture = new_texture
+		if info != null:
+			new_text = FileAccess.open("res://Assets/Infos/%s.txt" % name, FileAccess.READ)
+			new_text = new_text.get_as_text()
+			info.visible = true
+			infotext.text = new_text
 		
--- Scenes/main_menu.gd
+++ Scenes/main_menu.gd
@@ -3,7 +3,6 @@ extends Node2D
 @onready var text_edit: TextEdit = $CanvasLayer/Informationen
 
 
-
 func _on_button_pressed() -> void:
 	if text_edit.visible:
 		text_edit.visible = false
