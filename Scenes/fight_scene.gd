aa02fe017d2006098a2ceec95a13622f4c49db5d
gixerl <72729679+gixerl@users.noreply.github.com>
2025-06-18 17:51:17 +0200

    Punkte Anzeige, Timer reset between Questions, Timer runout = enemy Point

--- Scenes/fight_scene.gd
+++ Scenes/fight_scene.gd
@@ -17,7 +17,37 @@ var Quiz_Questions = preload("res://Assets/quizfragen.json")
 @onready var schätz_input_field: LineEdit = $"SchätzFrage/schätz_input_field"
 @onready var question_schätz : Label = $"SchätzFrage/Question"
 
+#Punkte
+@onready var points_1: Panel = $Points_1
+@onready var points_2: Panel = $Points_2
+@onready var points_win: Panel = $Points_Win
+@onready var points_4: Panel = $Points_4
+@onready var points_5: Panel = $Points_5
 
+
+var punkte_self = 0
+var punkte_gegner = 0
+
+func check_points():
+	if(punkte_self == 1):
+		points_1.modulate = Color("#00FF00")
+	if(punkte_self == 2):
+		points_2.modulate = Color("#00FF00")
+	if(punkte_self == 3):
+		points_win.modulate = Color("#00FF00")
+		await get_tree().create_timer(3.0).timeout
+		victory()
+		
+	if(punkte_gegner == 1):
+		points_5.modulate = Color("#FF0000")
+	if(punkte_gegner == 2):
+		points_4.modulate = Color("#FF0000")
+	if(punkte_gegner == 3):
+		points_win.modulate = Color("#FF0000")
+		await get_tree().create_timer(3.0).timeout
+		loss()
+
+#Questions		
 var Questions = []
 func _ready() -> void:
 	timer.start()
@@ -34,6 +64,7 @@ func _process(delta: float) -> void:
 	
 
 var attacked_Country = "Deutschland"
+var difficulty = "mittel"
 var Questions_MultipleChoice = []
 var min = 0
 var max
@@ -41,6 +72,9 @@ var Result
 var Margin
 
 func set_Question():
+	check_points()
+	timer.stop()
+	timer.start()
 	if(Questions[0]["klassifizierung"]["typ"] == "Multiple Choice"):
 		set_new_MultipleChoice(Questions[0])
 		multiple_choice_panel.visible = true
@@ -54,12 +88,12 @@ func set_Question():
 
 func new_Questions():
 	var question = Quiz_Questions.data
-	for fragen in question[attacked_Country]["einfach"]:
+	for fragen in question[attacked_Country][difficulty]:
 		Questions.append(fragen)
 	Questions.shuffle()
 func new_MultipleChoice():
 	var question = Quiz_Questions.data
-	for fragen in question[attacked_Country]["einfach"]:
+	for fragen in question[attacked_Country][difficulty]:
 		if(fragen["klassifizierung"]["typ"] == "Multiple Choice"):
 			Questions_MultipleChoice.append(fragen)
 	max = Questions_MultipleChoice.size()
@@ -80,24 +114,40 @@ func set_new_MultipleChoice(MultipleChoice_question):
 func _on_Button1_pressed() -> void:
 	if(button.text == str(Result)):
 		print("yeah")
+		punkte_self += 1
+		set_Question()
+	else:
+		punkte_gegner += 1
 		set_Question()
 
 
 func _on_button_2_pressed() -> void:
 	if(button_2.text == str(Result)):
 		print("yeah")
+		punkte_self += 1
+		set_Question()
+	else:
+		punkte_gegner += 1
 		set_Question()
 
 
 func _on_button_3_pressed() -> void:
 	if(button_3.text == str(Result)):
 		print("yeah")
+		punkte_self += 1
+		set_Question()
+	else:
+		punkte_gegner += 1
 		set_Question()
 
 
 func _on_button_4_pressed() -> void:
 	if(button_4.text == str(Result)):
 		print("yeah")
+		punkte_self += 1
+		set_Question()
+	else:
+		punkte_gegner += 1
 		set_Question()
 
 var Questions_Schätzfrage = []
@@ -112,7 +162,6 @@ func new_schätzFrage():
 
 func set_new_schätzFrage(Schätzfrage):
 	schätz_input_field.grab_focus()
-	var randome_num = randi_range(min, max - 1)
 	question_schätz.text = Schätzfrage["frage"]
 	Result = Schätzfrage["antwort"]
 	Margin = Schätzfrage["klassifizierung"]["margin_of_error"]
@@ -123,6 +172,7 @@ func _on_schätz_input_field_text_submitted(answer: String) -> void:
 	if(within_margin_of_error(answer,Result,Margin)):
 		schätz_input_field.text = ""
 		print("yeah")
+		punkte_self += 1
 		set_Question()
 	else:
 		schätz_input_field.text = ""
@@ -142,3 +192,15 @@ func within_margin_of_error(ans,result,margin):
 		var upperMargin = result + float(margin)
 		if(lowerMargin<= float(ans) and float(ans) <= upperMargin):
 			return true
+
+
+func victory():
+	pass
+
+func loss():
+	pass
+
+
+func _on_timer_timeout() -> void:
+	punkte_gegner += 1
+	set_Question()
