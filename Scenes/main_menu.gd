e2154cd7b1f343660385e9e1910c7ba47df2c88f
gixerl <72729679+gixerl@users.noreply.github.com>
2025-06-23 18:02:18 +0200

    added countries?????? what

--- Scenes/main_menu.gd
+++ Scenes/main_menu.gd
@@ -1,10 +1,12 @@
 extends Node2D
+@onready var soundtrack: AudioStreamPlayer2D = $Soundtrack
 
 @onready var text_edit: TextEdit = $CanvasLayer/Informationen
 @onready var victory: CanvasLayer = $Victory
 
 func _ready():
 	if get_tree().current_scene.name == "Selection_screen":
+		soundtrack.process_mode = Node.PROCESS_MODE_ALWAYS
 		get_node("CanvasLayer/Map Country Picker/Settings").position = Vector2(974, 40)
 		get_node("CanvasLayer/Map Country Picker/Options").position = Vector2(-954, -615)
 		var characters = get_tree().root.get_node("Selection_screen/CanvasLayer/Characters")
